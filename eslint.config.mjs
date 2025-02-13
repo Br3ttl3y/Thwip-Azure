8f75767e4582b2863ecbf702cb7cc5488ad756b6
Brett Carroll <brett.carroll@doh.wa.gov>
2025-02-13 11:20:17 -0800

    Added lint-staged and husky.  Added commit to test.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,23 +2,29 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import pluginReact from "eslint-plugin-react";
 import pluginJest from "eslint-plugin-jest";
+import pluginPrettier from "eslint-plugin-prettier";
+import pluginConfigPrettier from "eslint-config-prettier";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   {
-    files: ["**/*.{js,mjs,cjs,jsx}"]
+    files: ["**/*.{js,mjs,cjs,jsx}"],
   },
   {
-    languageOptions: { 
+    ignores: ["coverage", "node_modules"],
+  },
+  {
+    languageOptions: {
       globals: {
         ...globals.browser,
         ...globals.react,
         ...globals.node,
         ...globals.jest,
-      }
+      },
     },
     plugins: {
-      'jest' : pluginJest,
+      jest: pluginJest,
+      prettier: pluginPrettier,
     },
     rules: {
       "jest/no-disabled-tests": "warn",
@@ -30,4 +36,5 @@ export default [
   },
   pluginJs.configs.recommended,
   pluginReact.configs.flat.recommended,
+  pluginConfigPrettier,
 ];
--- tests/dummycode.test.js
+++ tests/dummycode.test.js
@@ -1,5 +1,9 @@
-const sum = require("../src/dummycode");
+const dummyCode = require("../src/dummyCode");
 
 test("Runs dummy sum function: 1+2=3", () => {
-    expect(sum(1,2)).toBe(3);
+  expect(dummyCode.sum(1, 2)).toBe(3);
+});
+
+test("Runs dummy difference function: 2-1=1", () => {
+  expect(dummyCode.difference(2, 1)).toBe(1);
 });
